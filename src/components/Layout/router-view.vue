3b3f54f07747456f7de5d82cfd9c6df3b219cf3f
Eagune 胡义晋 <huyj52@cn.midea.com>
2021-09-15 20:36:24 +0800

    feat: 添加管理端路由和页面， fix #26

---
+++ src/components/Layout/router-view.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <keep-alive :max="5">
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view>
+  </div>
+</template>
---
+++ src/router/management/index.js
@@ -0,0 +1,16 @@
+const RouterView = () => import("@/components/Layout/router-view")
+
+export default [
+  {
+    path: "/management",
+    component: RouterView,
+    redirect: "/management/home",
+    children: [
+      {
+        path: "home",
+        name: "home",
+        component: () => import("@/views/management/home"),
+      },
+    ],
+  },
+]
