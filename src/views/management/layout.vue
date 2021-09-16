f22dbb86c3ad16f8d2516d0175b4d914dbff712d
Eagune 胡义晋 <huyj52@cn.midea.com>
2021-09-16 09:33:00 +0800

    feat: 显示管理端logo，fix #33

--- src/views/management/layout.vue
+++ src/views/management/layout.vue
@@ -1,14 +1,22 @@
 <template>
-  <m-layout-pro></m-layout-pro>
+  <m-layout-pro
+    :logo="logoSrc">
+  </m-layout-pro>
 </template>
 
 <script>
 import mLayoutPro from "@/components/common/m-layout-pro"
+import logo from '@/assets/images/management/logo.png'
 
 export default {
   name: "ManagementLayout",
   components: {
     mLayoutPro,
   },
+  data () {
+    return {
+      logoSrc: logo
+    }
+  }
 }
 </script>
