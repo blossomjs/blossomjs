bd1b1adfcc032b92b791bbf6601d7346584ae660
Eagune 胡义晋 <huyj52@cn.midea.com>
2021-09-16 09:04:03 +0800

    feat: 左侧菜单根据数据生成, fix #31

--- src/components/common/m-layout-pro/components/layout-menu/index.vue
+++ src/components/common/m-layout-pro/components/layout-menu/index.vue
@@ -1,24 +1,22 @@
 <template>
-  <el-menu>
-    <el-sub-menu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-sub-menu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <template #title>导航二</template>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <template #title>导航三</template>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
-  </el-menu>
+  <div>
+    <template v-for="navMenu in navMenus">
+      <el-menu-item v-if="!navMenu.children" :key="navMenu.id" :index="navMenu.id">
+        <template #title>{{ navMenu.name }}</template>
+      </el-menu-item>
+      <el-sub-menu v-else :key="navMenu.id" :index="navMenu.id">
+        <template #title>{{ navMenu.name }}</template>
+        <layout-menu :navMenus="navMenu.children"></layout-menu>
+      </el-sub-menu>
+    </template>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "layout-menu",
+  props: {
+    navMenus: Array,
+  },
+}
+</script>
