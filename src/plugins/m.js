7ff543a2cb7ad8623d92639f5ba989b2aff75f59
Eagune 胡义晋 <huyj52@cn.midea.com>
2021-09-28 13:09:25 +0800

    feat: 菜单树及菜单详情, fix #49

--- src/plugins/m.js
+++ src/plugins/m.js
@@ -3,9 +3,11 @@ import mLayoutPage from "@/components/common/m-layout-page"
 import mPanel from "@/components/common/m-panel"
 import mPanelGroup from "@/components/common/m-panel-group"
 import mTable from "@/components/common/m-table"
+import mForm from "@/components/common/m-form"
+import mFormItem from "@/components/common/m-form-item"
 
 export default (app) => {
-  ;[mLayoutPro, mLayoutPage, mPanelGroup, mPanel, mTable].forEach((item) => {
+  ;[mLayoutPro, mLayoutPage, mPanelGroup, mPanel, mTable, mForm, mFormItem].forEach((item) => {
     app.component(item.name, item)
   })
 }
