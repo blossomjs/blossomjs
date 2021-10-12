456a41f8f7420e4de604ab1af7f57364869e1249
Eagune 胡义晋 <huyj52@cn.midea.com>
2021-10-12 20:29:02 +0800

    feat: 封装表格组件，fix #52

--- src/views/management/system/user/index.vue
+++ src/views/management/system/user/index.vue
@@ -1,16 +1,7 @@
 <template>
   <m-layout-page>
     <m-panel>
-      <el-table :data="tableData" stripe style="width: 100%" border>
-        <el-table-column prop="userId" label="编号"> </el-table-column>
-        <el-table-column prop="profileImg" label="头像"> </el-table-column>
-        <el-table-column prop="name" label="昵称"> </el-table-column>
-        <el-table-column prop="username" label="用户名"> </el-table-column>
-        <el-table-column prop="email" label="邮箱"> </el-table-column>
-        <el-table-column prop="enableStatus" label="状态"> </el-table-column>
-        <el-table-column prop="gender" label="性别"> </el-table-column>
-        <el-table-column prop="userType" label="用户类型"> </el-table-column>
-      </el-table>
+      <m-table :columns="table.columns" :data="table.data"></m-table>
     </m-panel>
   </m-layout-page>
 </template>
@@ -24,14 +15,23 @@ export default {
     return {
       tableData: [],
       table: {
-        columns: [],
+        columns: [
+          { prop: "userId", label: "编号" },
+          { prop: "profileImg", label: "头像" },
+          { prop: "name", label: "昵称" },
+          { prop: "username", label: "用户名" },
+          { prop: "email", label: "邮箱" },
+          { prop: "enableStatus", label: "状态" },
+          { prop: "gender", label: "性别" },
+          { prop: "userType", label: "用户类型" },
+        ],
         data: [],
       },
     }
   },
   async created() {
     const res = await req("getUserList")
-    this.tableData = res.records
+    this.table.data = res.records
   },
 }
 </script>
